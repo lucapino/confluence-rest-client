b2a4aba96be9ec0eebacfb44f94a8bb73a2fad43
Martin Böhmer <MartinBoehmer@users.noreply.github.com>
2017-04-19 21:51:37 +0200

    Extended RestException to provide response body containing valuable debug information (if available)

--- src/main/java/de/micromata/confluence/rest/core/misc/RestException.java
+++ src/main/java/de/micromata/confluence/rest/core/misc/RestException.java
@@ -4,21 +4,52 @@ import org.apache.http.StatusLine;
 import org.apache.http.client.methods.CloseableHttpResponse;
 
 import java.io.IOException;
+import java.io.StringReader;
+import org.apache.commons.io.IOUtils;
 
 /**
- * Created by cschulc on 01.07.2016.
+ * Authors: Christian Schulze (c.schulze@micromata.de), Martin Böhmer (mb@itboehmer.de)
+ * Created: 01.07.2016
+ * Modified: 19.04.2017
+ * Project: ConfluenceTransferPlugin
  */
 public class RestException extends Exception {
 
-    private int statusCode;
+    private final int statusCode;
 
-    private String reasonPhrase;
+    private final String reasonPhrase;
 
-    public RestException(CloseableHttpResponse response) throws IOException {
+    private String responseBody;
+
+    public RestException(CloseableHttpResponse response) {
         StatusLine statusLine = response.getStatusLine();
         this.statusCode = statusLine.getStatusCode();
         this.reasonPhrase = statusLine.getReasonPhrase();
-
-
+        try {
+            if (response.getEntity() != null && response.getEntity().getContent() != null) {
+                this.responseBody = IOUtils.toString(response.getEntity().getContent());
+            }
+        } catch (IOException ioe) {
+            // Noop. Should be logged.
+        }
     }
+
+    @Override
+    public String toString() {
+        boolean hasBody = (this.responseBody != null);
+        return "REST exception. Status: " + this.statusCode + ". Reason: " + this.reasonPhrase + ". Has body: " + hasBody;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public String getReasonPhrase() {
+        return reasonPhrase;
+    }
+
+    public String getResponseBody() {
+        return responseBody;
+    }
+
 }
